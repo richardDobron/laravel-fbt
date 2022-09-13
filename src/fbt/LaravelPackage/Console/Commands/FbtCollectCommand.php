fce9186884b509798b36721734093713090c3ea0
Richard Dobroň <office.etexweb@gmail.com>
2022-09-13 22:10:36 +0200

    Add command to collect FBT strings in *.blade.php

--- src/fbt/LaravelPackage/Console/Commands/FbtCollectCommand.php
+++ src/fbt/LaravelPackage/Console/Commands/FbtCollectCommand.php
@@ -3,7 +3,7 @@
 namespace fbt\LaravelPackage\Console\Commands;
 
 use fbt\FbtConfig;
-use fbt\Services\CollectFbtsService;
+use fbt\LaravelPackage\Services\CollectFbtsService;
 use Illuminate\Console\Command;
 
 class FbtCollectCommand extends Command
@@ -13,7 +13,8 @@ class FbtCollectCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'fbt:collect-fbts {--path= : The directory where you want to scan usages of fbt in php files}';
+    protected $signature = 'fbt:collect-fbts {--path=./app : The directory where you want to scan usages of fbt in php files}
+                                             {--views=true : Automatic compilation of the /resources/views directory.}';
 
     /**
      * The console command description.
@@ -24,11 +25,23 @@ class FbtCollectCommand extends Command
 
     public function handle(CollectFbtsService $collectFbtsService)
     {
+        if (! $this->option('path')) {
+            $this->error('--path option is required.');
+            return 1;
+        }
+
         try {
             $collectFbtsService->collectFromFiles(
                 FbtConfig::get('path'),
                 $this->option('path')
             );
+
+            if ($this->option('views') === 'true') {
+                $collectFbtsService->collectFromBladeFiles(
+                    FbtConfig::get('path'),
+                    base_path('resources/views/')
+                );
+            }
         } catch (\Exception $e) {
             $this->error($e->getMessage());
         }
---
+++ src/fbt/LaravelPackage/Services/CollectFbtsService.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace fbt\LaravelPackage\Services;
+
+use fbt\FbtConfig;
+use fbt\Runtime\Shared\FbtHooks;
+use function fbt\rsearch;
+
+class CollectFbtsService extends \fbt\Services\CollectFbtsService
+{
+    public function compileStatements(string $value)
+    {
+        $callback = function ($match) {
+            return isset($match[2]) ? '<?=' . $match[1] . $match[2] . '?>' : '';
+        };
+
+        return preg_replace_callback('/\B@(fbt)(\( ( (?>[^()]+) | (?2) )* \))?/x', $callback, $value);
+    }
+
+    public function collectFromBladeFiles(string $path, string $src)
+    {
+        $fbtDir = $path . '/';
+
+        if (! is_dir($fbtDir)) {
+            mkdir($fbtDir, 0755, true);
+        }
+
+        FbtConfig::set('path', $path);
+
+        foreach (rsearch($src, '/.blade.php$/') as $path) {
+            $this->collectFromOneFile($this->compileStatements(file_get_contents($path)), $path);
+        }
+
+        FbtHooks::storePhrases();
+    }
+}
